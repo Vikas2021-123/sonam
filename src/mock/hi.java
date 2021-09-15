8180713c853d64cdc0398fa156aa1b1a787e1a16
vikas kumar gupta <vikaspe2021@gmail.com>
2021-09-15 15:44:55 +0530

    hi hello

---
+++ src/mock/hi.java
@@ -0,0 +1,30 @@
+package mock;
+
+public class hi {
+	
+
+
+
+
+		
+
+			public static void main(String[] args) {
+			
+				System.out.println("This is my first git code");
+				
+				System.out.println("This is my second git code");
+
+				System.out.println("This is my third git code");
+
+				System.out.println("This is my fourth git code");
+
+				System.out.println("This is my fifth git code");
+				
+				System.out.println("This is my sixth git code");
+			
+		}
+
+	
+
+
+}
